c323f70f987877e52405ac7188ab50ea4daf36dd
Agam Shushan <agam.shushan@gmail.com>
2022-09-21 12:00:45 +0300

    bin

---
+++ TurtleBot.py
@@ -0,0 +1,30 @@
+import Consts
+import Trash
+
+
+bot_loc = [Consts.START_X, 0]
+
+
+def move_soldier(direction):
+    if direction == Consts.UP:
+        if bot_loc[1] > 0:
+            bot_loc[1] = bot_loc[1] - Consts.STEP
+    elif direction == Consts.DOWN:
+        if bot_loc[1] < Consts.TURTLE_AMOUNT - 1:
+            bot_loc[1] = bot_loc[1] + Consts.STEP
+    elif direction == Consts.RIGHT:
+        if bot_loc[0] < Consts.ARRAY_SIZE - 1:
+            bot_loc[0] = bot_loc[0] + Consts.STEP
+    elif direction == Consts.LEFT:
+        if bot_loc[0] > 0:
+            bot_loc[0] = bot_loc[0] - Consts.STEP
+
+
+def if_on_trash():
+    trash = Trash.get_trash()
+    if trash[bot_loc[1]][bot_loc[0]] != Consts.EMPTY:
+        Trash.remove_trash(bot_loc)
+
+
+def get_loc():
+    return bot_loc
