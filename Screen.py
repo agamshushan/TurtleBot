931cc296d836a25d880fbc2c502f720c7a94a24d
RonyGildor <ronygildor@gmail.com>
2022-09-21 13:59:53 +0300

    screen

--- Screen.py
+++ Screen.py
@@ -1,45 +1,72 @@
 import pygame
 import Consts
 import Trash
+import TurtleBot
 
 WINDOW = Consts.WINDOW
+robot_pixel_x, robot_pixel_y = 0, 0
 
 def move_robot(keys_pressed):
     direction = ""
+    global robot_pixel_x
+    global robot_pixel_y
     if keys_pressed[pygame.K_LEFT]:  # LEFT
-        direction = Consts.LEFT
+        if TurtleBot.move_bot(Consts.LEFT):
+            for i in range(Consts.ROBOT_WIDTH):
+                robot_pixel_x -= 1
+                update_starter_screen()
     if keys_pressed[pygame.K_RIGHT]:  # RIGHT
-        direction = Consts.RIGHT
+        if TurtleBot.move_bot(Consts.RIGHT):
+            for i in range(Consts.ROBOT_WIDTH):
+                robot_pixel_x += 1
+                update_starter_screen()
     if keys_pressed[pygame.K_UP]:  # UP
-        direction = Consts.UP
+        if TurtleBot.move_bot(Consts.UP):
+            for i in range(Consts.ROBOT_HEIGHT):
+                robot_pixel_x -= 1
+                update_starter_screen()
     if keys_pressed[pygame.K_DOWN]:  # DOWN
-        direction = Consts.DOWN
-    return direction
+        if TurtleBot.move_bot(Consts.DOWN):
+            for i in range(Consts.ROBOT_HEIGHT):
+                robot_pixel_x += 1
+                update_starter_screen()
 
-def slide_robot():
-    while()
 
-
+def index_to_pixels(x_index, y_index):
+    return (x_index * 50, y_index * 100 + 100)
 
 
 def update_starter_screen():
     WINDOW.blit(Consts.BACKGROUND_IMG, (0, 0))
     trash_array = Trash.get_trash()
+    global robot_pixel_y
+    global robot_pixel_x
 
-    for row in trash_array:
+    for i in range(Consts.TURTLE_AMOUNT):
+        if i % 2 != 0:
+            row = trash_array[-1::-1]
+            turtle_data = pygame.transform.rotate(pygame.transform.scale(Consts.TURTLE_IMAGE, (
+                Consts.ROBOT_WIDTH, Consts.ROBOT_HEIGHT)), 90)
+            turtle = pygame.Rect(index_to_pixels(0, i),
+                                 (Consts.ROBOT_WIDTH, Consts.ROBOT_HEIGHT))
+            WINDOW.blit(turtle_data, (turtle.x, turtle.y))
+        else:
+            turtle_data = pygame.transform.rotate(pygame.transform.scale(Consts.TURTLE_IMAGE, (
+                Consts.ROBOT_WIDTH, Consts.ROBOT_HEIGHT)), 270)
+            turtle = pygame.Rect(index_to_pixels(19, i),
+                                 (Consts.ROBOT_WIDTH, Consts.ROBOT_HEIGHT))
+            WINDOW.blit(turtle_data, (turtle.x, turtle.y))
+        for j in range(len(row)):
+            if j != Consts.EMPTY:
+                data = pygame.transform.scale(Consts.TRASH_IMAGES[j], (
+                Consts.TRASH_WIDTH, Consts.TRASH_HEIGHT))
+                trash = pygame.Rect((index_to_pixels(j, i)[0], index_to_pixels(j, i)[1] + 25), (Consts.TRASH_WIDTH, Consts.TRASH_HEIGHT))
+                WINDOW.blit(data, (trash.x, trash.y))
 
 
 
-    ship_data = pygame.transform.scale(Consts.SHIP_IMAGE, (Consts.SHIP_WIDTH_IMAGE, Consts.SHIP_HEIGHT_IMAGE))
-    ship = pygame.Rect(index_to_pixels(Consts.SHIP_LOCATION), (Consts.SHIP_WIDTH_IMAGE, Consts.SHIP_HEIGHT_IMAGE))
-    WINDOW.blit(ship_data, (ship.x, ship.y))
-
-    player_data = pygame.transform.scale(Consts.PLAYER_IMG, (Consts.PLAYER_WIDTH, Consts.PLAYER_HEIGHT))
-    player = pygame.Rect(index_to_pixels(player_index), (Consts.PLAYER_WIDTH, Consts.PLAYER_HEIGHT))
-    WINDOW.blit(player_data, (player.x, player.y))
-
-    guard_data = pygame.transform.scale(Consts.GUARD_IMG, (Consts.PLAYER_WIDTH, Consts.PLAYER_HEIGHT))
-    guard = pygame.Rect(index_to_pixels(guard_index),(Consts.PLAYER_WIDTH, Consts.PLAYER_HEIGHT))
-    WINDOW.blit(guard_data, (guard.x, guard.y))
+    robot_data = pygame.transform.scale(Consts.TURTLE_BOT_IMAGE, (Consts.ROBOT_WIDTH, Consts.ROBOT_HEIGHT))
+    robot = pygame.Rect((robot_pixel_x, robot_pixel_y), (Consts.ROBOT_WIDTH, Consts.ROBOT_HEIGHT))
+    WINDOW.blit(robot_data, (robot.x, robot.y))
 
     pygame.display.update()
